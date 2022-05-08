23bdc7f0c2c20818fde3ee519df69cd5643fe794
Z <zhenisbusy@qq.com>
2022-05-08 23:24:21 +0800

    开发配置相关，锁定开发启动端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 开发服务器选项
+  server: {
+    host: 'localhost',
+    port: 3050,
+    strictPort: true
+  },
+  resolve: {
+    // 文件系统路径别名（声明此项后尽量使用绝对路径）
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
